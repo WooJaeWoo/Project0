ff84f922b502993955bdfe49e3a6ee0ff1ba3719
Jaewoo Woo <gewoose@gmail.com>
2016-02-22 18:38:00 +0900

    Fix max logic

--- webapp/modules/score.js
+++ webapp/modules/score.js
@@ -3,50 +3,50 @@ var magazine = ["design", "happy", "luxury", "mywedding", "mens", "enfant"];
 var scoreMatrix = {
 	a1: { //gender
 		"M": [1, 2, 3, 4, 5, 6],
-		"F": [1, 2, 3, 4, 5, 6]
+		"F": [4, 3, 6, 5, 2, 1]
 	},
 	a2: { // age
-		"10": [1, 2, 3, 4, 5, 6],
+		"10": [4, 3, 6, 5, 2, 1],
 		"20": [1, 2, 3, 4, 5, 6],
-		"30": [1, 2, 3, 4, 5, 6],
-		"40": [1, 2, 3, 4, 5, 6],
-		"50": [1, 2, 3, 4, 5, 6],
+		"30": [6, 4, 2, 3, 1, 5],
+		"40": [6, 4, 2, 3, 1, 5],
+		"50": [4, 3, 6, 5, 2, 1],
 		"60": [1, 2, 3, 4, 5, 6]
 	},
 	a3: { // married
-		"true": [1, 2, 3, 4, 5, 6],
+		"true": [4, 3, 6, 5, 2, 1],
 		"false": [1, 2, 3, 4, 5, 6]
 	},
 	a4: { // atHome
 		"A": [1, 2, 3, 4, 5, 6],
-		"B": [1, 2, 3, 4, 5, 6],
-		"C": [1, 2, 3, 4, 5, 6]
+		"B": [4, 3, 6, 5, 2, 1],
+		"C": [6, 4, 2, 3, 1, 5]
 	},
 	a5: { // needs
 		"money": [1, 2, 3, 4, 5, 6],
-		"bag": [1, 2, 3, 4, 5, 6],
+		"bag": [4, 3, 6, 5, 2, 1],
 		"book": [1, 2, 3, 4, 5, 6],
 		"friend": [1, 2, 3, 4, 5, 6],
-		"love": [1, 2, 3, 4, 5, 6]
+		"love": [4, 3, 6, 5, 2, 1]
 	},
 	a6: { // interiorRatio
-		"A": [1, 2, 3, 4, 5, 6],
-		"B": [1, 2, 3, 4, 5, 6],
+		"A": [4, 3, 6, 5, 2, 1],
+		"B": [6, 4, 2, 3, 1, 5],
 		"C": [1, 2, 3, 4, 5, 6]
 	},
 	a7: { // color
 		"red": [1, 2, 3, 4, 5, 6],
-		"yellow": [1, 2, 3, 4, 5, 6],
+		"yellow": [6, 4, 2, 3, 1, 5],
 		"green": [1, 2, 3, 4, 5, 6],
-		"blue": [1, 2, 3, 4, 5, 6],
-		"purple": [1, 2, 3, 4, 5, 6]
+		"blue": [4, 3, 6, 5, 2, 1],
+		"purple": [6, 4, 2, 3, 1, 5]
 	},
 	a8: { // culture
 		"DIY": [1, 2, 3, 4, 5, 6],
-		"cook": [1, 2, 3, 4, 5, 6],
-		"travel": [1, 2, 3, 4, 5, 6],
-		"exercise": [1, 2, 3, 4, 5, 6],
-		"exhibition": [1, 2, 3, 4, 5, 6]
+		"cook": [4, 3, 6, 5, 2, 1],
+		"travel": [6, 4, 2, 3, 1, 5],
+		"exercise": [4, 3, 6, 5, 2, 1],
+		"exhibition": [6, 4, 2, 3, 1, 5]
 	}
 };
 
@@ -84,7 +84,9 @@ scoreMod.prototype.calculate = function(answer) {
 	this.addScores(scoreMatrix.a7[answer.a7]);
 	this.addScores(scoreMatrix.a8[answer.a8[0]]);
 	this.addScores(scoreMatrix.a8[answer.a8[1]]);
-	this.addScores(scoreMatrix.a8[answer.a8[2]]);
+}
+scoreMod.prototype.getScore = function() {
+	return this.score;
 }
 scoreMod.prototype.getResult = function() {
 	var maxIdx = -1;
@@ -96,9 +98,12 @@ scoreMod.prototype.getResult = function() {
 	for (var i = 0; i < this.score.length; i++) {
 		if (this.score[i] > maxVal) {
 			nextMaxIdx = maxIdx;
+			nextMaxVal = maxVal;
 			maxIdx = i;
+			maxVal = this.score[i];
 		} else if (this.score[i] > nextMaxVal) {
 			nextMaxIdx = i;
+			nextMaxVal = this.score[i];
 		}
 	}
 	return { first: magazine[maxIdx], second: magazine[nextMaxIdx] };
