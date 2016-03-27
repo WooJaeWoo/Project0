7f701f811b3c221e042db0aca977ddb13748d804
Jaewoo Woo <gewoose@gmail.com>
2016-03-28 01:32:37 +0900

    Data ajax

--- webapp/public/js/data.js
+++ webapp/public/js/data.js
@@ -1,6 +1,31 @@
 $(document).on("ready", function() {
-	console.log("hello, world!");
+	Data.init();
 });
 
 
 
+var Data = {
+	init: function() {
+		$("aside").on("click", "li", function(event) {
+			var li = $(event.currentTarget);
+			$("h1").text(li.text());
+			this.getData(li.data("col"));
+		}.bind(this));
+	},
+	getData: function(col) {
+		$.ajax({
+			url: "/data/" + col,
+			method: "POST",
+			dataType: "json",
+			success: function(result) {
+				console.log(result);
+			},
+			error: function(error) {
+				console.log("Ajax Error");
+				if (error) {
+					console.log(error);
+				}
+			}
+		});
+	}
+}
